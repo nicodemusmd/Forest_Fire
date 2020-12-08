3b53806a256b9d3386f681557af1c9a446fea55e
Michael Nicodemus <nicodemus.michaeld@gmail.com>
2020-12-07 18:59:55 -0800

    initialized repository with files

---
+++ MyLibrary.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn import preprocessing
+from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import explained_variance_score as EV
+from sklearn.metrics import mean_absolute_error as MAE
+
+from sklearn.model_selection import train_test_split as TTS
+from sklearn.metrics import mean_squared_error as MSE
+
+
